c7d3f7c09963d86988932cf534d6b061a19bac2a
Mi Zhou <mzhou@student.42.fr>
2025-12-02 14:19:56 +0100

    add(ex00): add all files for ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,41 @@
+#include "ClapTrap.hpp"
+#include <string>
+#include <iostream>
+
+void displayInfos(ClapTrap& object)
+{
+	std::cout << "-----------------------------------------------------\n";
+	std::cout << "name is " << object.getName_() << std::endl;
+	std::cout << "hitPoints_ is " << object.getHitPoints_() << std::endl;
+	std::cout << "energyPoints_ is " << object.getEnergyPoints_() << std::endl;
+	std::cout << "attackDammage_ is " << object.getAttackDammage_() << std::endl;
+	std::cout << "-----------------------------------------------------\n";
+}
+
+int main()
+{
+	ClapTrap dog("dog");
+	ClapTrap cat("cat");
+
+	displayInfos(dog);
+	displayInfos(cat);
+	dog.attack(cat.getName_());
+	displayInfos(dog);
+	displayInfos(cat);
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	displayInfos(dog);
+	displayInfos(cat);
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+	dog.attack(cat.getName_());
+
+	return (0);
+}
