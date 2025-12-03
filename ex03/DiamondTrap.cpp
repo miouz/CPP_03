3a767ca0596af6c5bbf732145561ad18751aa10c
Mi Zhou <mzhou@student.42.fr>
2025-12-03 21:13:48 +0100

    update(ex03): update diamondtrap class

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -1,9 +1,12 @@
 #include "DiamondTrap.hpp"
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
 #include <iostream>
 
 DiamondTrap::DiamondTrap():
 	ClapTrap("_clap_name"), FragTrap(), ScavTrap()
 {
+	std::cout << "DiamondTrap defaut constructor called\n";
 	name_ = "";
 	hitPoints_ = FragTrap::hitPoints_;
 	energyPoints_ = ScavTrap::energyPoints_;
@@ -14,27 +17,28 @@ DiamondTrap::DiamondTrap():
 DiamondTrap::DiamondTrap(const std::string& name):
 	ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name)
 {
+	std::cout << "DiamondTrap constructor called\n";
 	name_ = name;
 	hitPoints_ = FragTrap::hitPoints_;
 	energyPoints_ = ScavTrap::energyPoints_;
 	attackDammage_ = FragTrap::attackDammage_;
+	std::cout <<" attackDammage_ from FragTrap is : "<< FragTrap::attackDammage_<<std::endl;
+	std::cout <<" attackDammage_ from ScavTrap is : "<< ScavTrap::attackDammage_<<std::endl;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+	std::cout << "DiamondTrap destructor called\n";
 }
 
 void DiamondTrap::attack(const std::string& target)
 {
-	if (this->energyPoints_ > 0 && this->hitPoints_ > 0)
-	{
-		std::cout << "~DiamondTrap " << this->name_ << " attacks " << target 
-			<< ", causing " << this->attackDammage_ << " points of damage!\n";
-		energyPoints_--;
-	}
-	else
-	 	std::cout << "DiamondTrap " << this->name_ << " can not attack anymore ORZ\n";
+	ScavTrap::attack(target);
 }
 
 void DiamondTrap::whoAmI()
 {
 	std::cout << "I'm the greatest DiamondTrap " << name_ << std::endl
 		<< "My ClapTrap name is " << ClapTrap::name_ << std::endl
-		<< "now you may kiss my toe\n";
+		<< "I'm wierd\n";
 }
--- ex03/DiamondTrap.hpp
+++ ex03/DiamondTrap.hpp
@@ -12,6 +12,7 @@ class DiamondTrap : public FragTrap, public ScavTrap
 	public:
 	DiamondTrap();
 	DiamondTrap(const std::string& name);
+	~DiamondTrap();
 	void attack(const std::string& target);
 	void whoAmI();
 };
